3812842952dff2d0fb9ebe83e58d43959a33b525
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-07-28 11:33:27 -0400

    chore: add metadata

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,11 +1,18 @@
+import type { Metadata } from 'next';
+
 import { EXPLORATIONS } from '@/lib/constants/site';
 
 import ChatExamples from '@/components/pages/chat/examples';
 import InfoStack from '@/components/templates/info-stack';
 
-export default function Chat() {
-  const CHAT_EXPLORATION = EXPLORATIONS[0];
+const CHAT_EXPLORATION = EXPLORATIONS[0];
 
+export const metadata: Metadata = {
+  title: CHAT_EXPLORATION.name,
+  description: CHAT_EXPLORATION.description,
+};
+
+export default function Chat() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-4 pb-16 md:px-24 md:py-16">
       <InfoStack
