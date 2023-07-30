eb5d0078da8ed1e63d139d71c06148ec88e2ae45
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-07-30 12:07:44 -0400

    chore: make drawer unfocusable

--- components/pages/chat/switch-address-drawer-internal.tsx
+++ components/pages/chat/switch-address-drawer-internal.tsx
@@ -19,8 +19,8 @@ const ChatSwitchAddressDrawerInternal: FC<ChatSwitchAddressDrawerInternalProps> 
     <Drawer.Root>
       <Drawer.Trigger asChild>{trigger}</Drawer.Trigger>
       <Drawer.Portal>
-        <Drawer.Overlay className="fixed inset-0 z-overlay h-screen w-screen bg-black/40" />
-        <Drawer.Content className="fixed bottom-0 left-0 right-0 z-popover mx-auto flex flex-col rounded-t-xl border-t border-gray-6 bg-gray-2 p-4 sm:max-w-[432px] sm:border-x">
+        <Drawer.Overlay className="fixed inset-0 z-overlay h-screen w-screen bg-black/40 focus:outline-none" />
+        <Drawer.Content className="fixed bottom-0 left-0 right-0 z-popover mx-auto flex flex-col rounded-t-xl border-t border-gray-6 bg-gray-2 p-4 focus:outline-none sm:max-w-[432px] sm:border-x">
           {children}
         </Drawer.Content>
       </Drawer.Portal>
